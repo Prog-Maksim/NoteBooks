9e0a8ae592dffc6b8659a8a6c0da116f6c2047e1
Maksim Beloglazov <maksimbel20170@gmail.com>
2024-05-19 17:42:06 +0300

    реализация передачи парамертров программе

--- NoteBooks/App.xaml.cs
+++ NoteBooks/App.xaml.cs
@@ -1,29 +1,105 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using NoteBooks.Models;
+
 
 namespace NoteBooks
 {
-
-    public partial class App : Application
+    public partial class App
     {
         void App_Startup(object sender, StartupEventArgs e)
         {
-            bool startMinimized = false;
-            for (int i = 0; i != e.Args.Length; ++i)
+            if (e.Args.Length == 0)
             {
-                if (e.Args[i] == "/StartMinimized")
+                sturtUpProgram(WindowState.Normal);
+            }
+            else if (e.Args[0] == "/StartMinimized")
+            {
+                sturtUpProgram(WindowState.Minimized);
+            }
+            else if (e.Args[0] == "/OpenSticker")
+            {
+                int openSticker = openThumbtackStickers();
+                if (openSticker == 0) Application.Current.Shutdown();
+            }
+            else
+            {
+                try
                 {
-                    startMinimized = true;
-                }
+                    if (File.Exists(e.Args[0]))
+                    {
+                        string name = new FileInfo(e.Args[0]).Name.Replace(".rtf", "");
 
-                MessageBox.Show(e.Args[0]);
+                        if (!Directory.Exists(Path.Combine(ClassRegistry.PathOpenStickers, $"~{name}")))
+                        {
+                            if (Sticker.checkIsAvailabilityFile(name))
+                            {
+                                openStickers(name);
+                            }
+                            else
+                            {
+                                Sticker.addNewStickerContextMenu(new FileInfo(e.Args[0]));
+                                openStickers(name);
+                            }
+                        }
+                        else
+                            Application.Current.Shutdown();
+                    }
+                    else
+                        Application.Current.Shutdown();
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message + "\n" + exception.StackTrace, "StickyNotes", MessageBoxButton.OK, MessageBoxImage.Error);
+                    Application.Current.Shutdown();
+                }
             }
-        
-            MainWindow mainWindow = new MainWindow();
-            if (startMinimized)
+        }
+
+        private void sturtUpProgram(WindowState state)
+        {
+            string procName = Process.GetCurrentProcess().ProcessName;
+            Process[] processes = Process.GetProcessesByName(procName);
+            if (processes.Length > 1)
+                Application.Current.Shutdown();
+            try
             {
-                mainWindow.WindowState = WindowState.Minimized;
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.WindowState = state;
+                mainWindow.Show();
             }
-            mainWindow.Show();
+            catch (InvalidOperationException) { }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+            
+            openThumbtackStickers();
+        }
+
+        private int openThumbtackStickers()
+        {
+            StickersList stickersList = Sticker.getAllDataSticker();
+            int openSticker = 0;
+
+            foreach (var sticker in stickersList.Stickers)
+            {
+                if (sticker.StickerThumbtack && !Directory.Exists(Path.Combine(ClassRegistry.PathOpenStickers, $"~{sticker.Name}")))
+                {
+                    openStickers(sticker.Name);
+                    openSticker++;
+                }
+            }
+
+            return openSticker;
+        }
+
+        private void openStickers(string name)
+        {
+            var sticky = new Sticky(name);
+            sticky.Show();
         }
     }
 }
