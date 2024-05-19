e64882e4efd5f94a7458ed8e5d380c6288b224e4
Maksim Beloglazov <maksimbel20170@gmail.com>
2024-05-19 17:46:31 +0300

    обновление моделей для конфиг файлов

--- NoteBooks/Models/Config.cs
+++ NoteBooks/Models/Config.cs
@@ -7,10 +7,9 @@ public class Config
     public themeNameStyle Theme { get; set; }
     public bool DeleteNotification { get; set; }
     public bool AutoStart { get; set; }
-    public string SavePathData { get; set; }
     public StickerSetings StickerSetings { get; set; }
 
     [JsonConstructor]
-    public Config(themeNameStyle theme, bool deleteNotification, bool autoStart, string savePathData, StickerSetings stickerSetings) =>
-        (Theme, DeleteNotification, AutoStart, SavePathData, StickerSetings) = (theme, deleteNotification, autoStart, savePathData, stickerSetings);
+    public Config(themeNameStyle theme, bool deleteNotification, bool autoStart, StickerSetings stickerSetings) =>
+        (Theme, DeleteNotification, AutoStart, StickerSetings) = (theme, deleteNotification, autoStart, stickerSetings);
 }
--- NoteBooks/Models/Sticker.cs
+++ NoteBooks/Models/Sticker.cs
@@ -5,15 +5,16 @@ namespace NoteBooks.Models;
 public class Sticker
 {
     public string Name { get; set; }
-    public string Color { get; set; }
+    public int Color { get; set; }
     public DateTime DateStart { get; set; }
     public DateTime CurrentDateUpdate { get; set; }
     public bool StickerThumbtack { get; set; }
     public bool StickerFavorite { get; set; }
-    public bool StickerIsOpen { get; set; }
+    public string StickerCurrentPath { get; set; }
+    public StickyData StickyData { get; set; }
 
-    public Sticker(string name, string color, DateTime dateStart, DateTime currentDateUpdate, bool stickerThumbtack,
-        bool stickerFavorite, bool stickerIsOpen) =>
-        (Name, Color, DateStart, CurrentDateUpdate, StickerThumbtack, StickerFavorite, StickerIsOpen) = (name, color,
-            dateStart, currentDateUpdate, stickerThumbtack, stickerFavorite, stickerIsOpen);
+    public Sticker(string name, int color, DateTime dateStart, DateTime currentDateUpdate, bool stickerThumbtack,
+        bool stickerFavorite, string stickerCurrentPath, StickyData stickyData) =>
+        (Name, Color, DateStart, CurrentDateUpdate, StickerThumbtack, StickerFavorite, StickerCurrentPath, StickyData) = (name, color,
+            dateStart, currentDateUpdate, stickerThumbtack, stickerFavorite, stickerCurrentPath, stickyData);
 }
